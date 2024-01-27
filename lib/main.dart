80507317492a5a5380c560bbe80c2dc151ecd197
Nikhil Narayanan <javakingxi@gmail.com>
2024-01-27 20:50:05 +0530

    ✨ Replace and Modify CalendarView

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gaming_tracker/pages/CalendarPage.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,23 +15,12 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          brightness: Brightness.dark,
+          seedColor: const Color(0xFFF23453),
+        ),
+        textTheme: GoogleFonts.sourceCodeProTextTheme(),
       ),
       home: const CalendarPage(),
     );
--- lib/pages/CalendarPage.dart
+++ lib/pages/CalendarPage.dart
@@ -1,19 +1,29 @@
 // ignore_for_file: file_names
 
 import 'package:flutter/material.dart';
+import 'package:calendar_date_picker2/calendar_date_picker2.dart';
 
 class CalendarPage extends StatelessWidget {
   const CalendarPage({super.key});
   @override
   Widget build(BuildContext context) {
+    //Current date to initialize the calendar
+    DateTime today = DateTime.now();
+    List<DateTime?> _dates = [today];
     return SafeArea(
       child: Scaffold(
+        backgroundColor: const Color(0xFF1E1E1E),
         body: Center(
-          child: CalendarDatePicker(
-              initialDate: DateTime(2024),
-              firstDate: DateTime(2024),
-              lastDate: DateTime(2025),
-              onDateChanged: (changed) {}),
+          child: CalendarDatePicker2(
+            config: CalendarDatePicker2Config(
+              dayTextStyle: const TextStyle(
+                  color: Colors.white, fontWeight: FontWeight.w700),
+              selectedDayTextStyle: const TextStyle(
+                  color: Colors.black, fontWeight: FontWeight.w700),
+            ),
+            value: _dates,
+            onValueChanged: (dates) => _dates = dates,
+          ),
         ),
       ),
     );
